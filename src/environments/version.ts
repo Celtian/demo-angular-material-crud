ae8278a7947e903557f36a3c030d5cb5788ed71c
Dominik Hladík <dominik.hladik@coex.cz>
2023-03-08 20:33:59 +0100

    chore(version): version file added

---
+++ src/environments/version.ts
@@ -0,0 +1,16 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED!
+/* eslint-disable */
+export const VERSION = {
+  "version": "0.0.0",
+  "date": "2023-03-08T19:31:38.262Z",
+  "author": {
+    "name": "Dominik Hladík",
+    "email": "dominik.hladik@seznam.cz",
+    "url": "https://github.com/Celtian"
+  },
+  "git": {
+    "branch": "master",
+    "commit": "9d337398813b15d4323e02ad44c2ffa257340653"
+  }
+};
+/* eslint-enable */
