869f380f09ade2d5ee19ac429eafd5df1a275b7f
Dominik Hladík <dominik.hladik@seznam.cz>
2023-08-13 10:29:03 +0200

    feat(fix): small unused fix

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,5 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
-/*
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
-*/
-
 // eslint-disable-next-line no-console
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
